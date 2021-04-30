a8440a52a40a4d123668da4d7fb92f286af22165
tyler <taehyun9202@hotmail.com>
2021-04-30 01:23:25 -0400

    first commit

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p>
+        Question: {questionNumber} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }}></p>
+      <div>
+        {answers.map((answer) => (
+          <div>
+            <button disabled={userAnswer} value={answer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
